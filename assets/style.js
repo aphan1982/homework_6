da2c98e8628b50f27cfe7272e6493cd5589df166
aphan <phantasticvs@yahoo.com>
2020-04-02 11:33:14 -0600

    Added pseudocode for style.js

---
+++ assets/style.js
@@ -0,0 +1,16 @@
+// Get current time (moment.js?)
+
+// Get city from search bar (input form-control)
+
+// When search button clicked, it drops information into the current city field:
+
+// Get current temperature
+// Get current RH
+// Get current wind speed
+// Get UV index
+
+// Figure a way to color-code the UV index according to conditions
+
+// Figure a way to get five-day data for temperature and RH.
+
+// Save current search in search history (localStorage) and bring up last ten searches.
