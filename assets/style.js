ad30681d92872fd0886d89d96f7788e10dc623f0
aphan <phantasticvs@yahoo.com>
2020-04-03 16:42:40 -0600

    Made a wrapper for .ready().

--- assets/style.js
+++ assets/style.js
@@ -12,63 +12,65 @@ function setTime() {
 };
 setTime();
 
+// VARIABLES
 var APIKey_OWM = "016e0c84a66372bfe43d6b8df53c6531";
 var queryURL_OWM = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIKey_OWM;
 var searchInput = "";
 
-// INITIALIZATION--makes sure that no buttons are being rendered if no search history exists in local storage; if a search history exists, up to ten most recent searches will be rendered:
-function initSearch() {
-  var buttons = $(".history");
-  var searchedCities = JSON.parse(localStorage.getItem("cities"));
-  buttons.empty();
-  buttons.addClass("d-none");
-  if (!searchedCities) {
-    $("#cityBtn1").text("no recent history");
-    $("#cityBtn1").removeClass("d-none");
-    return;
-  } else {
-    renderHistory();
-  }
-};
-initSearch();
-
-// SET HISTORY--takes user input (search cities) and stores it in local storage:
-function setSearchHistory() {
-  var city = $("#cityInput").val().trim();
-  if (!city) {
-    return;
-  } else {
+$(document).ready(function() {
+  // INITIALIZATION--makes sure that no buttons are being rendered if no search history exists in local storage; if a search history exists, up to ten most recent searches will be rendered:
+  function initSearch() {
+    var buttons = $(".history");
     var searchedCities = JSON.parse(localStorage.getItem("cities"));
+    buttons.empty();
+    buttons.addClass("d-none");
     if (!searchedCities) {
-      searchedCities = [];
-    }
-    // sets search history in descending order from most to least recent:
-    searchedCities.unshift(city);
-    localStorage.setItem("cities", JSON.stringify(searchedCities));
-  }
-};
-
-// RENDER HISTORY--displays each search entry as a clickable button:
-function renderHistory() {
-  var searchBtns = [$("#cityBtn1"), $("#cityBtn2"), $("#cityBtn3"), $("#cityBtn4"), $("#cityBtn5"), $("#cityBtn6"), $("#cityBtn7"), $("#cityBtn8"), $("#cityBtn9"), $("#cityBtn10")];
-  searchedCities = JSON.parse(localStorage.getItem("cities"));
-  for (i = 0; i < searchBtns.length; i++) {
-    if (searchedCities[i] === undefined) {
+      $("#cityBtn1").text("no recent history");
+      $("#cityBtn1").removeClass("d-none");
       return;
     } else {
-      searchBtns[i].text(searchedCities[i]);
-      searchBtns[i].removeClass("d-none");
+      renderHistory();
     }
-  }
-};
+  };
+  initSearch();
 
-$("#genCity").on("click", function(event) {
-  event.preventDefault();
-  setSearchHistory();
-  $("#cityInput").val("");
-  renderHistory();
+  // SET HISTORY--takes user input (search cities) and stores it in local storage:
+  function setSearchHistory() {
+    var city = $("#cityInput").val().trim();
+    if (!city) {
+      return;
+    } else {
+      var searchedCities = JSON.parse(localStorage.getItem("cities"));
+      if (!searchedCities) {
+        searchedCities = [];
+      }
+      // sets search history in descending order from most to least recent:
+      searchedCities.unshift(city);
+      localStorage.setItem("cities", JSON.stringify(searchedCities));
+    }
+  };
+
+  // RENDER HISTORY--displays each search entry as a clickable button:
+  function renderHistory() {
+    var searchBtns = [$("#cityBtn1"), $("#cityBtn2"), $("#cityBtn3"), $("#cityBtn4"), $("#cityBtn5"), $("#cityBtn6"), $("#cityBtn7"), $("#cityBtn8"), $("#cityBtn9"), $("#cityBtn10")];
+    searchedCities = JSON.parse(localStorage.getItem("cities"));
+    for (i = 0; i < searchBtns.length; i++) {
+      if (searchedCities[i] === undefined) {
+        return;
+      } else {
+        searchBtns[i].text(searchedCities[i]);
+        searchBtns[i].removeClass("d-none");
+      }
+    }
+  };
+
+  $("#genCity").on("click", function(event) {
+    event.preventDefault();
+    setSearchHistory();
+    $("#cityInput").val("");
+    renderHistory();
+  });
 });
-
 
 
 
