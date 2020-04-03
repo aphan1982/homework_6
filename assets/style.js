e793948bd2ead7d8d3406a97a35a6466ffae8cc9
aphan <phantasticvs@yahoo.com>
2020-04-03 16:38:35 -0600

    Added descriptions in style.js, cleaned up whitespaces, added a conditional to prevent the setting of empty/null data into local storage, cleaned up null conditionals with a bang operator.

--- assets/style.js
+++ assets/style.js
@@ -12,12 +12,17 @@ function setTime() {
 };
 setTime();
 
+var APIKey_OWM = "016e0c84a66372bfe43d6b8df53c6531";
+var queryURL_OWM = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIKey_OWM;
+var searchInput = "";
+
+// INITIALIZATION--makes sure that no buttons are being rendered if no search history exists in local storage; if a search history exists, up to ten most recent searches will be rendered:
 function initSearch() {
   var buttons = $(".history");
   var searchedCities = JSON.parse(localStorage.getItem("cities"));
   buttons.empty();
   buttons.addClass("d-none");
-  if (searchedCities === null) {
+  if (!searchedCities) {
     $("#cityBtn1").text("no recent history");
     $("#cityBtn1").removeClass("d-none");
     return;
@@ -27,23 +32,23 @@ function initSearch() {
 };
 initSearch();
 
-
-
-var APIKey_OWM = "016e0c84a66372bfe43d6b8df53c6531";
-var searchInput = "";
-
-var queryURL_OWM = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIKey_OWM;
-
+// SET HISTORY--takes user input (search cities) and stores it in local storage:
 function setSearchHistory() {
   var city = $("#cityInput").val().trim();
-  var searchedCities = JSON.parse(localStorage.getItem("cities"));
-  if (searchedCities === null) {
-    searchedCities = [];
+  if (!city) {
+    return;
+  } else {
+    var searchedCities = JSON.parse(localStorage.getItem("cities"));
+    if (!searchedCities) {
+      searchedCities = [];
+    }
+    // sets search history in descending order from most to least recent:
+    searchedCities.unshift(city);
+    localStorage.setItem("cities", JSON.stringify(searchedCities));
   }
-  searchedCities.unshift(city);
-  localStorage.setItem("cities", JSON.stringify(searchedCities));
 };
 
+// RENDER HISTORY--displays each search entry as a clickable button:
 function renderHistory() {
   var searchBtns = [$("#cityBtn1"), $("#cityBtn2"), $("#cityBtn3"), $("#cityBtn4"), $("#cityBtn5"), $("#cityBtn6"), $("#cityBtn7"), $("#cityBtn8"), $("#cityBtn9"), $("#cityBtn10")];
   searchedCities = JSON.parse(localStorage.getItem("cities"));
@@ -57,13 +62,16 @@ function renderHistory() {
   }
 };
 
-
 $("#genCity").on("click", function(event) {
   event.preventDefault();
   setSearchHistory();
   $("#cityInput").val("");
   renderHistory();
 });
+
+
+
+
 
 // Get city from search bar (input form-control)
 
